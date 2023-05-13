06f5e9a407bcf7faac7396ff755439e139848dd9
anarmh <anarmh@code.edu.az>
2023-05-13 22:27:39 +0400

    add  practice

---
+++ FiorelloFront/FiorelloFront/Controllers/ShopController.cs
@@ -0,0 +1,34 @@
+﻿using FiorelloFront.Data;
+using FiorelloFront.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FiorelloFront.Controllers
+{
+    public class ShopController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ShopController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+
+            IEnumerable<Product> products = await _context.Products.Include(m => m.ProductImages).Where(m => !m.SoftDelete).Take(4).ToListAsync();
+
+            int count = await _context.Products.Where(m => !m.SoftDelete).CountAsync();
+
+            ViewBag.Count=count;
+            return View(products);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ShowMoreOrLess(int skip)
+        {
+            IEnumerable<Product> products = await _context.Products.Include(m => m.ProductImages).Where(m => !m.SoftDelete).Skip(skip).Take(4).ToListAsync();
+            return PartialView("_ProductsPartial",products);
+        }
+    }
+}
